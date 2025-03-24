ffc23d265dbc8a0f2d0a4899ed94b7eac9581a63
INPATHTAMILAN M <inbarepute@gmail.com>
2025-03-24 17:49:02 +0530

    Refactor event update email notification template and signal handler

--- zenapp/apps.py
+++ zenapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ZenappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'zenapp'
+
+    def ready(self):
+        import zenapp.signals.event_update_signal
--- zenapp/signals/event_update_signal.py
+++ zenapp/signals/event_update_signal.py
@@ -1,14 +1,13 @@
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
 from django.dispatch import receiver
-from django.db.models.signals import post_save  # Import post_save signal
-from zenapp.models import Event, EventRegistration  # Import the Event and EventRegistration models
+from django.db.models.signals import post_save  
+from zenapp.models import Event, EventRegistration 
+from authapp.utils.email_sender import send_email  
+
 
 
 @receiver(post_save, sender=Event)
 def send_event_update_email(sender, instance, created, **kwargs):
-    """Send email notifications when an event is updated."""
+    
     if created:  # Ignore if event is newly created
         return
 
@@ -21,24 +20,19 @@ def send_event_update_email(sender, instance, created, **kwargs):
         user = registration.user
 
         # Render the email template with dynamic content
-        html_content = render_to_string("emails/event_update.html", {
-            "user_name": user.get_full_name(),
+        content = {
+            "user_name": user.email,
             "event_title": instance.title,
             "event_start_date": instance.start_date,
             "event_end_date": instance.end_date,
             "event_start_time": instance.start_time,
             "event_end_time": instance.end_time,
-            "event_location": "Online" if not instance.session_link else instance.session_link,
-            "event_link": f"https://yourdomain.com/events/{instance.id}/"
-        })
+            "event_location": "Online",
+            "event_link": instance.session_link
+            
+        }
+        template = "event_update_notify.html"
+        send_email("Event Update", user.email, template, content)
         
-        text_content = strip_tags(html_content)  # Convert HTML to plain text
-
-        email = EmailMultiAlternatives(
-            subject=f"Update: Event '{instance.title}' has been modified",
-            body=text_content,
-            from_email="no-reply@yourdomain.com",
-            to=[user.email]
-        )
-        email.attach_alternative(html_content, "text/html")  # Attach HTML version
-        email.send(fail_silently=True)
+        
+        
