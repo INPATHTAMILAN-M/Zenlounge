589b6d2024cec3c7bbf4fc7be3936c03457cac2b
INPATHTAMILAN <inbarepute@gmail.com>
2025-05-02 06:36:40 +0000

    Disable pagination in CountryViewSet list method

--- authapp/viewset/country_viewset.py
+++ authapp/viewset/country_viewset.py
@@ -5,3 +5,7 @@ from authapp.serializers import CountrySerializer
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+    def list(self, request, *args, **kwargs):
+        self.pagination_class = None  # Disable pagination
+        return super().list(request, *args, **kwargs)
