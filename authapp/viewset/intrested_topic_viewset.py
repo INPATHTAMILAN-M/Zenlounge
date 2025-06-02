b38423a16ebb9297d7a4692a58c6463ff612a002
INPATHTAMILAN <inbarepute@gmail.com>
2025-06-02 09:27:24 +0000

    Remove ordering by 'topic' from IntrestedTopic queryset

--- authapp/viewset/intrested_topic_viewset.py
+++ authapp/viewset/intrested_topic_viewset.py
@@ -3,7 +3,7 @@ from ..models import  IntrestedTopic
 from ..serializers import IntrestedTopicSerializer
 
 class IntrestedTopicViewSet(viewsets.ModelViewSet):
-    queryset = IntrestedTopic.objects.all().order_by('topic')
+    queryset = IntrestedTopic.objects.all()
     serializer_class = IntrestedTopicSerializer
     # permission_classes = [permissions.IsAuthenticated]
     http_method_names = ['get', 'post', 'patch', 'delete']
