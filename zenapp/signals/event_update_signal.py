e560a8061d2831cee82d153b7e5fd531467ff6b4
INPATHTAMILAN M <inbarepute@gmail.com>
2025-06-17 14:39:53 +0530

    Enhance email templates and add event creation email notification functionality

--- zenapp/signals/event_update_signal.py
+++ zenapp/signals/event_update_signal.py
@@ -2,6 +2,39 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save  
 from zenapp.models import Event, EventRegistration 
 from authapp.utils.email_sender import send_email  
+from authapp.models import CustomUser
+
+
+@receiver(post_save, sender=Event)
+def send_event_creation_email(sender, instance:Event, created, **kwargs):
+
+    registered_users = CustomUser.objects.all()
+
+    if not created:  
+        return
+
+    if not registered_users.exists():
+        return
+
+    for registration in registered_users:
+        user = registration.user
+
+        # Render the email template with dynamic content
+        content = {
+            "user_name": user.first_name + " " + user.last_name,
+            "event_title": instance.title,
+            "event_start_date": instance.start_date,
+            "event_end_date": instance.end_date,
+            "event_start_time": instance.start_time,
+            "event_end_time": instance.end_time,
+            "event_category": instance.lounge_type.name,
+            "event_location": "Online",
+            "event_link": instance.session_link
+            
+        }
+        template = "new-event-creation.html"
+        send_email("Event Created", user.email, template, content)
+
 
 
 
@@ -27,6 +60,7 @@ def send_event_update_email(sender, instance:Event, created, **kwargs):
             "event_end_date": instance.end_date,
             "event_start_time": instance.start_time,
             "event_end_time": instance.end_time,
+            "event_category": instance.lounge_type.name,
             "registration_id": registration.registration_id,
             "registration_status": registration.registration_status,
             "event_location": "Online",
